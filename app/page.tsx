75f6d3d85e256d51c6c291dfdb96738d32219024
dreameutopia <13077995107@163.com>
2025-07-02 14:50:54 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import WaitingRoom from "../waiting-room"
+import { Suspense } from "react"
 
 export default function Page() {
-  return <WaitingRoom />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WaitingRoom />
+    </Suspense>
+  )
 }
